78df040d645acf29daf6bb663d4628a2482399f8
iamustiii <140164115+iamustiii@users.noreply.github.com>
2024-07-19 07:31:11 +0100

    Remove all files

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"X4ITizqWyAhzEjuFNxR7FLAmOnGRBNG9eBSkw8VCuFc=\"\n}"
--- app/dir/page.tsx
+++
@@ -1,7 +0,0 @@
-import { BottomSheet } from "../components/BottomSheet";
-
-export default function Test() {
-    return (
-        <BottomSheet />
-    )
-}
